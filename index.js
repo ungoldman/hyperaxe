98a85536d1d708de5529c76e8c4cd9f62c18e348
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-10-04 08:12:36 +0200

    Add new minimal `index.js`

---
+++ index.js
@@ -0,0 +1,6 @@
+var {createFactory, getFactory} = require('./factory')
+var h = require('hyperscript')
+
+module.exports = createFactory(h)
+module.exports.createFactory = createFactory
+module.exports.getFactory = getFactory
